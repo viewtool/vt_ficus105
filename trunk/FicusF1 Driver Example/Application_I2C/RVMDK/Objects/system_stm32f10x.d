129712aee5021d4cc9040c0b1dfb8c6afd41718d
ViewTool-Jerry <ViewTool-Jerry@viewtool.com>
2019-11-28 19:20:12 +0800

    1.delete debug message and DebugConfig message

--- trunk/FicusF1 Driver Example/Application_I2C/RVMDK/Objects/system_stm32f10x.d
+++
@@ -1,8 +0,0 @@
-.\objects\system_stm32f10x.o: ..\CMSIS\Device\ST\STM32F10x\Source\Templates\system_stm32f10x.c
-.\objects\system_stm32f10x.o: E:\1.Program_Software\Keil_v5\Arm\Packs\Keil\STM32F1xx_DFP\2.0.0\Device\Include\stm32f10x.h
-.\objects\system_stm32f10x.o: .\RTE\_Flash\RTE_Components.h
-.\objects\system_stm32f10x.o: ..\CMSIS\Include\core_cm3.h
-.\objects\system_stm32f10x.o: E:\1.Program_Software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_stm32f10x.o: ..\CMSIS\Include\core_cmInstr.h
-.\objects\system_stm32f10x.o: ..\CMSIS\Include\core_cmFunc.h
-.\objects\system_stm32f10x.o: E:\1.Program_Software\Keil_v5\Arm\Packs\Keil\STM32F1xx_DFP\2.0.0\Device\Include\system_stm32f10x.h
--- trunk/FicusF1 Driver Example/Application_SPI/RVMDK/Objects/startup_stm32f10x_cl.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f10x_cl.o: ..\CMSIS\Device\ST\STM32F10x\Source\Templates\arm\startup_stm32f10x_cl.s
